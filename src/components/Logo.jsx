9bbc061b4e2379628a34f1d804042a6806d61d23
Sidhanth Pandey <sidhanthpandey@192.168.1.6>
2025-04-22 10:05:40 +0530

    repo setup

---
+++ src/components/Logo.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import logoDefault from "../../public/logos/logo-default.png";
+import logoDark from "../../public/logos/logo-dark.png";
+import Image from "next/image";
+
+export default function Logo() {
+    return (
+        <div className="w-auto">
+            <Link href={""} >
+                <div className='text-black flex items-center text-3xl dark:text-white font-medium tracking-tighter'>
+                    <Image src={logoDefault} alt="ZeroByte Logo" className='h-12 w-12 dark:hidden' />
+                    <Image src={logoDark} alt="ZeroByte Logo" className='h-12 w-12 hidden dark:block' />
+                    <span className='ml-[-12px]'>byte</span>
+                </div>
+            </Link>
+        </div>
+    );
+}
