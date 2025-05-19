5c0d9ac27728672d7eedad9c6e1697a618586afe
Sidhanth Pandey <sidhanthpandey@192.168.1.10>
2025-05-19 16:35:16 +0530

    Fix: overflow hero component and minor ui changes

--- src/components/Subtitle.jsx
+++ src/components/Subtitle.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Subtitle = ({ text, className }) => {
   return (
-    <h2 className={`${className} w-fit backdrop-blur-lg`}>{text}</h2>
+    <h2 className={`${className} w-fit backdrop-blur-xs rounded-lg`}>{text}</h2>
   )
 }
 
